67e2e5071c5bcd3eb7e52d688173a98e785eb821
briseidy-torres-velez <58743853+briseidy-torres-velez@users.noreply.github.com>
2020-03-11 22:22:31 -0600

    services

---
+++ OnlineStoreTeam2/CurrencyService/Controllers/currencyserviceController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using CurrencyService.models;
+
+namespace CurrencyService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class currencyserviceController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<double> GetExchangeRate(CurrencyChange currencyChange)
+        {
+
+
+            //Examples:
+            //from = "EUR"
+            //to = "USD"
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("https://api.exchangeratesapi.io/latest");
+
+                var response = await client.GetAsync($"?base={currencyChange.CurrencyCode}&symbols={currencyChange.CurrencyType}&amount={currencyChange.Units}");
+
+                var stringResult = await response.Content.ReadAsStringAsync();
+
+                var dictResult = JsonConvert.DeserializeObject<dynamic>(stringResult);
+
+                var exchangeValue = dictResult.rates[$"{currencyChange.CurrencyType}"];
+
+                var convert = exchangeValue * currencyChange.Units;
+
+                return convert;
+
+
+            }
+        }
+
+    }
+}
