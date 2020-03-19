827a15cfa21d4b3482e647d57b723476f1ccbcbf
briseidy-torres-velez <58743853+briseidy-torres-velez@users.noreply.github.com>
2020-03-19 15:13:46 -0600

    cambios shipping

--- OnlineStoreTeam2/ShippingService/Controllers/shippingController.cs
+++ OnlineStoreTeam2/ShippingService/Controllers/shippingController.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-
+using Newtonsoft.Json.Linq;
+using ShippingService.models;
 
 namespace ShippingService.Controllers
 {
@@ -15,14 +16,43 @@ namespace ShippingService.Controllers
     public class shippingController : ControllerBase
     {
         //TODO
-        [HttpPost]
-        public double GetShippingServices( double total)
-        {                                           
-                var shippingCost = total * .15;
+        //[Route("estimate")]
+        [HttpPost("estimate/{total}")]
+        public dynamic ShippingCostServices(double total)
+        { 
+            var ShippingCost = total * .20;
 
-                return shippingCost;
+            var myjson = new
+            {
+                calculatedShippingCost = ShippingCost
+            };
+            var json = JsonConvert.SerializeObject(myjson);
 
             
+            JObject json2 = JObject.Parse(json);
+
+            return json2;
+
+         
+        }
+
+        [Route("tracking")]
+        [HttpPost]
+        public string TrackingID(Shipping shipping)
+        {
+            string trackingid = shipping.ZipCode + DateTime.Today.ToString("ddMMyyyy") + DateTime.Now.ToString("HHmmss");
+
+
+
+            var myjsondata = new
+            {
+                trackingID = trackingid
+            };
+            string json = JsonConvert.SerializeObject(myjsondata);
+
+
+
+            return json;
         }
     }
 }
